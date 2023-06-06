d1d34347bf867287f83dab48bf6c76363ceb4553
Page <phayley2@kumc.edu>
2023-06-06 15:11:13 -0500

    SEC fixes
    Corrected orientation of plot_array and addition to colormap fn

--- SEC/plot_array.m
+++ SEC/plot_array.m
@@ -16,62 +16,67 @@ end
 % set variables for color mapping
 low_lim = 0.8;
 up_lim = 20;
-mid = [4,10];
+mid = 4;
+cut_off = []; % set to low_lim or up_lim if values should be grayed out
 %% Plot SEC data as an array
 [C,sel] = select_data(L,DataStructure,1); % run selection function
 clearvars L DataStructure
 for i = 1:size(C.Blocks,1)
-    [fig,p,ax,xc,yc] = create_array_fig(C.Probe_Flip(i));
+    [fig,pat,ax,xc,yc] = create_array_fig(C.Probe_Flip(i));
     hold on
     reArr = [4 3 2 1 10 9 8 7 6 5 16 15 14 13 12 11 27 28 29 30 31 32 21 22 23 ...
         24 25 26 17 18 19 20 36 35 34 33 42 41 40 39 38 37 48 47 46 45 44 43 59 ...
-        60 61 62 63 64 53 54 55 56 57 58 49 50 51 52]; % key for spatially plotting channels
+        60 61 62 63 64 53 54 55 56 57 58 49 50 51 52]'; % key for spatially plotting channels
     id = reArr(1:32) - 1; % actual channel titles
-    id = [id id];
+    id = [id;id];
     txt_id = compose('Ch%03d',id);
     txt = text(xc,yc,txt_id,'Interpreter','none');
-    title(C.Blocks(i)); % ???
+    title = text(5,7,char(C.Blocks(i)),'Interpreter','none','FontSize',18); % not sure why title call is not working
     if C.Probe_Flip(i) == 1 % flip arrays
         text(10,6.5,'P1','FontSize',14);
         text(3,6.5,'P2','FontSize',14);
-        reArr = [reArr(33:64) reArr(1:32)];
-        ch_ref = [33:64,1:32];
-        xc = [xc(33:64),xc(1:32)];
-        yc = [yc(33:64),yc(1:32)];
     else
         text(3,6.5,'P1','FontSize',14);
         text(10,6.5,'P2','FontSize',14);
-        ch_ref = [1:64];
     end
+    arr = [repmat(("P1"),32,1);repmat(("P2"),32,1)];
     pk_latency = zeros(64,1);
     pk_rate = zeros(64,1);
-    chPlot = table(ch_ref',pk_latency,pk_rate,'VariableNames',{'ch_ref','pk_latency','pk_rate'});
+    rand_sig = zeros(64,1);
+    mean_rate = cell(64,1);
+    chPlot = table(arr,txt_id,reArr,pk_latency,pk_rate,rand_sig,mean_rate,...
+        'VariableNames',{'arr','ch','ch_ref','pk_latency','pk_rate','rand_sig','mean_rate'});
     if ~exist(fullfile(C.Dir{i},[char(C.Blocks(i))],[char(C.Blocks(i)),'_refstats.mat']))
-        cntup = 0;
-        for ii = ch_ref % number of channels
-            cntup = cntup + 1;
+        for ii = 1:numel(reArr) % channels in order of plotting P1 to P2
             chID = txt_id{ii};
-            if ii <= 32
+            r = reArr(ii);
+            if r <= 32 
                 load(fullfile(C.Dir{i},[char(C.Blocks(i))],[char(C.Blocks(i)),'_StimTriggeredStats_ChannelSpiking_RandomBlanked'],...
                     [char(C.Blocks(i)),'_ChannelStats_P1_',chID,'.mat']));
             else
                 load(fullfile(C.Dir{i},[char(C.Blocks(i))],[char(C.Blocks(i)),'_StimTriggeredStats_ChannelSpiking_RandomBlanked'],...
                     [char(C.Blocks(i)),'_ChannelStats_P2_',chID,'.mat']));
             end
-            chPlot.pk_latency(cntup) = Latency_ms;
-            chPlot.pk_rate(cntup) = MaxSpikeRate;
+            chPlot.pk_latency(ii) = Latency_ms;
+            chPlot.pk_rate(ii) = MaxSpikeRate;
+            if isempty(p) % work around for now
+                chPlot.rand_sig(ii) = 1;
+            else
+            chPlot.rand_sig(ii) = p;
+            end
+            chPlot.mean_rate{ii} = MeanSpikeRate;
         end
         save(fullfile(C.Dir{i},[char(C.Blocks(i))],[char(C.Blocks(i)),'_refstats.mat']),'chPlot');
     else
         load(fullfile(C.Dir{i},[char(C.Blocks(i))],[char(C.Blocks(i)),'_refstats.mat']),'chPlot');
     end
     % assign patch colors based on latency
-    p.FaceColor = 'flat';
-    p.FaceVertexCData = chPlot.pk_latency;
-    cmap = set_colormap(low_lim,up_lim,mid);
+    pat.FaceColor = 'flat';
+    pat.FaceVertexCData = chPlot.pk_latency;
+    [cmap,bound] = set_colormap(low_lim,up_lim,mid,cut_off); 
     colormap(cmap);
     colorbar;
-    caxis([low_lim up_lim]);
+    caxis(bound);
     % id stim channel and label
     idxP = C.Stim_Probe(i);
     idxCh = C.Stim_Ch(i);
@@ -83,7 +88,8 @@ for i = 1:size(C.Blocks,1)
         stCh = pr_list(idxCh);
     end
     iCh = find(reArr == stCh);
-    text(xc(iCh),yc(iCh),'X','Fontweight','bold','Fontsize',32);
+    tStim = text(xc(iCh),yc(iCh),'X','Fontweight','bold','Fontsize',32);
+    
 end
 %% Plot against map data 
 % need to have run match_assays.m prior to this
--- set_colormap.m
+++ set_colormap.m
@@ -1,5 +1,5 @@
 %% Map colors for a colormap
-function [cmap] = set_colormap(low_lim,up_lim,mid)
+function [cmap,bound] = set_colormap(low_lim,up_lim,mid,cut_off)
 % sets a colormap based on the lower - upper limits of the data
 % can adjust the relative color change using mid
 % uses red/yellow/green scale for one midpoint
@@ -11,21 +11,26 @@ function [cmap] = set_colormap(low_lim,up_lim,mid)
 % mid; single for the mid-way yellow point 
 %
 % OUTPUT:
-% cmap; a color map
+% cmap; a colormap
+% bound; values for scaling caxis, usually low_lim/up_lim unless cut-off is indicated
 
-range = linspace(low_lim,up_lim,50);
-if ~exist('mid')
+% values for creating a colormap
+res = 50; % sets the number of values between the main shades
+range = linspace(low_lim,up_lim,res);
+if isempty(mid)
     mid = 25;
 end
+
+% fills colormap variable
 if numel(mid) == 1
     [~,match] = min(abs(range - mid));
-    cmap = zeros(50,3);
+    cmap = zeros(res,3);
     cmap(1,:) = [1 0 0];
     cmap(match,:) = [1 1 0];
     cmap(end,:) = [0 1 0];
     before = match;
     cB = linspace(0,1,before);
-    after = 50 - match;
+    after = res - match;
     cA = flip(linspace(0,1,after + 1));
     for i = 2:(before - 1)
         cmap(i,:) = [1 cB(i) 0];
@@ -37,7 +42,7 @@ if numel(mid) == 1
 elseif numel(mid) == 2
     [~,match1] = min(abs(range - mid(1)));
     [~,match2] = min(abs(range - mid(2)));
-    cmap = zeros(50,3);
+    cmap = zeros(res,3);
     cmap(1,:) = [1 0 0];
     cmap(match1,:) = [1 1 0];
     cmap(match2,:) = [0 1 0];
@@ -46,7 +51,7 @@ elseif numel(mid) == 2
     cB = linspace(0,1,before);
     middle = match2 - match1;
     cM = flip(linspace(0,1,middle + 1));
-    after = 50 - match2;
+    after = res - match2;
     cA2 = linspace(0,1,after + 1);
     cA1 = flip(cA2);
     for i = 2:(before - 1)
@@ -59,5 +64,19 @@ elseif numel(mid) == 2
     for i = 2:after
         fill = match2 + i - 1;
         cmap(fill,:) = [0 cA1(i) cA2(i)];
+    end
+end
+if isempty(cut_off)
+        bound = [low_lim up_lim];
+% adds a color to gray out values above/below a cut-off
+else
+    if cut_off == low_lim
+        cmap = [0 0 0; cmap];
+        hold = low_lim - ((low_lim + up_lim)/res);
+        bound = [hold up_lim];
+    elseif cut_off == up_lim
+        cmap = [cmap; 0 0 0];
+        hold = up_lim + ((low_lim + up_lim)/res); 
+        bound = [low_lim hold];
     end
 end
